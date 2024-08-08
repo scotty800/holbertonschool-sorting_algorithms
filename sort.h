e6c17cfc7f525937475b9db5a8ac1345fbd923ae
root <root@DESKTOP-SH55I7R>
2024-08-08 13:51:34 +0200

    modificaition

--- sort.h
+++ sort.h
@@ -24,7 +24,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void print_array1(const int *array, size_t size);
+/*void print_array1(const int *array, size_t size);*/
 void quick_sort_rec(int *array, int low, int high, size_t size);
 int lomutoPartition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
