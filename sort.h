38a3e7e6f0db5b5299d1d57e2c9b221bf22c7332
root <root@DESKTOP-SH55I7R>
2024-08-08 12:27:09 +0200

    add prototype

--- sort.h
+++ sort.h
@@ -26,6 +26,7 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array1(const int *array, size_t size);
 void quick_sort_rec(int *array, int low, int high, size_t size);
+int lomutoPartition(int *array, int low, int high, size_t size);
 void swap(int *a, int *b);
 
 #endif
